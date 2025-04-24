1a36b062f11911ee2fc03f0004d45fde32044a57
Ibahim1811 <meduardo033@gmail.com git config --global user.name>
2025-04-23 21:22:04 -0600

    Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Ibahim1811.py
@@ -0,0 +1,22 @@
+# Este es el link de pythom https://www.python.org/
+"""
+Asi se comenta en varias lineas
+Jaja
+"""
+
+Bariable = "Mi variable"
+Bariable_x2 = "otra variable"
+
+MY_CONSTANT = "CONSTANTE" # NO entendi a que se refiere commo por concención
+
+#Tipos de datos primitivos en python
+tdp_int = 1
+tdp_float = 3.1416
+tdp_bool = True # False
+tdp_string = " variables de texto"
+
+print ("¡Hola, Python!")
+print(type(tdp_int))
+print(type(tdp_float))
+print(type(tdp_bool))
+print(type(tdp_string))
